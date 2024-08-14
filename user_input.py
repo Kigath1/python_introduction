7e17b99b328d7482844435a71c84f29f7752998c
Kigath1 <dennismm502@gmail.com>
2024-08-14 23:11:30 +0200

    week 1 assignment commit

---
+++ user_input.py
@@ -0,0 +1,12 @@
+# input for name 
+name = input("Enter your name: ")
+
+# age input 
+age = input("Enter your age: ")
+
+# location input 
+location = input("Enter your prefered location: ")
+
+#  printing out ythe personalized message 
+print(f"Hello, y name is {name}. I am {age} years old and i live in {location}. ")
+
