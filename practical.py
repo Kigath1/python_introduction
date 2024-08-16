3e65bc0ad636b8b18f220ad4f68437f1efeafa0e
Kigath1 <dennismm502@gmail.com>
2024-08-16 12:35:08 +0200

    casted the values to strings

---
+++ practical.py
@@ -0,0 +1,30 @@
+# testing how variables work
+site1 = site2 = 'Power Learn Project'
+print(site1)
+print(site2)
+
+num1 = 5
+print(num1, 'is a type number of ', type(num1)) 
+
+num2 = 1+2j
+print(num2, 'is a type of number ', type(num2))
+
+# lists 
+practical = ['number', 'languages', 'lists']
+# accessing the list contents using array indexing 
+print(practical[0])
+print(practical[2])
+print(practical)
+
+# tuples in python consist of mixed up datatypes in lists 
+tripple = ('badbox', 1033, 20.4, 'terminal')
+print(tripple[2])
+
+capital_cities = {'kenya':'nairobi', 'somalia':'abudabi'}
+print(capital_cities['kenya'])
+
+numbers = [21,10,33,67]
+print (numbers)
+numbers.append(21)
+print(numbers)
+
--- user_input.py
+++ user_input.py
@@ -2,10 +2,18 @@
 name = input("Enter your name: ")
 
 # age input 
-age = input("Enter your age: ")
+age = int(input("Enter your age: "))
+# using an if statement to check the age input by the user 
+if age > 18 :
+    print(str(name), "is an adult")
+else :
+    print(str(name), "is not yet an adult")
+
+dec = float(input("Enter a decimal number: ")) 
+print("The number ", str(dec), "is a decimal number")
 
 # location input 
 location = input("Enter your prefered location: ")
 
 #  printing out ythe personalized message 
-print(f"Hello, y name is {name}. I am {age} years old and i live in {location}. ")
+print(f"Hello, my name is ",str(name), ". I am ",str(age), "years old and i live in ", str(location), ".")
